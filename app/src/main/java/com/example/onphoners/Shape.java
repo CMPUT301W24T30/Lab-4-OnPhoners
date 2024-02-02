ad959293ef3d772c7af872c160ae46710ac2f0ac
Jeff Weston <21jeffweston@gmail.com>
2024-02-02 12:28:21 -0700

    added shape string

--- app/src/main/java/com/example/onphoners/Shape.java
+++ app/src/main/java/com/example/onphoners/Shape.java
@@ -3,7 +3,8 @@ package com.example.onphoners;
 public abstract class Shape {
     public int x;
     public int y;
-    String color = "urmom";
+
+    String color = "skibidi";
 
     Shape(int x, int y) {
         this.x = x;
