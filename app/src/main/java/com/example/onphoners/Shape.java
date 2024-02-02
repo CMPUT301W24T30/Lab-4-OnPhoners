9534c802ebec6241a1e84156ca9f603a7bbdd7e9
Yifeng Chen <yifeng15@ualberta.ca>
2024-02-02 15:43:20 -0700

    Update color field in Shape

--- app/src/main/java/com/example/onphoners/Shape.java
+++ app/src/main/java/com/example/onphoners/Shape.java
@@ -3,7 +3,7 @@ package com.example.onphoners;
 public abstract class Shape {
     public int x;
     public int y;
-    String color = "urmom";
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
 
     Shape(int x, int y) {
         this.x = x;
