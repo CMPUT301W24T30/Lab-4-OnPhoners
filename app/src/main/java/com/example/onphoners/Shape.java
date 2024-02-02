ab2fbec9a0de6493f112a6270c5a41e44f5fcc5f
Logan <87048158+Logan9312@users.noreply.github.com>
2024-02-02 12:44:14 -0700

    Adding Evil Rectangle (#6)
    * Adding Evil Rectangle
    
    * Update build.gradle.kts
    
    * Update build.gradle.kts
    
    * Update build.gradle.kts
    
    * Update gradle-wrapper.properties
    
    * Update build.gradle.kts
    
    * Update build.gradle.kts
    
    * Adding Evil Rectangle
    
    * Adding Evil Rectangle
    
    * Resolving conflicts

--- app/src/main/java/com/example/onphoners/Shape.java
+++ app/src/main/java/com/example/onphoners/Shape.java
@@ -3,7 +3,8 @@ package com.example.onphoners;
 public abstract class Shape {
     public int x;
     public int y;
-    String color = "urmom";
+
+    String color = "Hyperbolic Orange";
 
     Shape(int x, int y) {
         this.x = x;
