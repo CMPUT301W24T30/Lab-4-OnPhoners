94aa7222918ae547f1dc3900a46fe8312f872ef4
Logan <lgntravis@gmail.com>
2024-02-01 23:16:22 -0700

    Resolving conflicts

--- app/src/main/java/com/example/onphoners/Shape.java
+++ app/src/main/java/com/example/onphoners/Shape.java
@@ -3,9 +3,6 @@ package com.example.onphoners;
 public abstract class Shape {
     public int x;
     public int y;
-    String color = "urmom";
-
-    String color = "Hyperbolic Orange";
 
     String color = "Hyperbolic Orange";
 
